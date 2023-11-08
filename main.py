db0065e26e03dace4f590928c280172e068dbb0d
alex <73740276+aahlexxx@users.noreply.github.com>
2023-11-08 10:23:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,13 +29,28 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 tabs_font_css = """
 <style>
+div[class*="stTextArea"] label {
+  font-size: 26px;
+  color: red;
+}
+
+div[class*="stTextInput"] label {
+  font-size: 26px;
+  color: blue;
+}
+
 div[class*="stNumberInput"] label {
   font-size: 26px;
   color: green;
 }
 </style>
 """
+
 st.write(tabs_font_css, unsafe_allow_html=True)
+
+st.text_area("Text area")
+st.text_input("Text input")
+st.number_input("Number input")
 
 st.title("Coffee Production Cost Calculator")
 st.write("---")
